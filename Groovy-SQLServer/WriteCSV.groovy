3a28de653d92b1f7997b68e1855ca9f4628b3987
Rohit Singh <c.ronnie.r@gmail.com>
2019-03-20 14:25:51 +0530

    Use FileWriter to generate .csv

--- Groovy-SQLServer/WriteCSV.groovy
+++ Groovy-SQLServer/WriteCSV.groovy
@@ -8,17 +8,17 @@ class WriteCSV
 	static void main(args) throws ClassNotFoundException, SQLException, IOException
 	{
 		final def JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-		final def JDBC_URL = "jdbc:sqlserver://IPKOLTOOLSSRV1;databaseName=SMSPHtndSFHI";
-		final def USERNAME = "a2aUser";
-		final def PASSWORD = "a2apassW";
+		final def JDBC_URL = "jdbc:sqlserver://____serverName____;databaseName=________________";
+		final def USERNAME = "_________";
+		final def PASSWORD = "_________";
 		
-		def choice, check = false, slNo, topic, status, date, query;
+		def check = false;
 
 		def sql = Sql.newInstance(JDBC_URL, USERNAME, PASSWORD, JDBC_DRIVER); // requesting a connection
 		
 		
 		def tableRows = []
-		sql.eachRow("SELECT * from [dbo].[RevReclasfRqstQueue]")
+		sql.eachRow("SELECT * from [dbo].[____tableName____]")
 		{
 			tp ->tableRows << tp.toString();
 			if(!check) // affect only once
@@ -28,7 +28,7 @@ class WriteCSV
 			println "No data found.\n"
 		else				
 		{
-			FileWriter fileWriter = new FileWriter("C:\\Users\\RS067558\\Documents\\RevReclasfRqstQueue.csv");
+			FileWriter fileWriter = new FileWriter("C:\\Users\\Rohit\\Documents\\____tableName____.csv");
 			tableRows.forEach { tp ->fileWriter.append( tp.toString()+"\n" )}
 			println "CSV file created successfully."
 			fileWriter.close();
